34d5f8fb1ea739075b7d79814d3a6dfdcf8d791b
bholmes-learning <bh.learning.studies@gmail.com>
2025-02-05 19:57:36 -0500

    Update afa-linux.tf

--- afa-linux.tf
+++ afa-linux.tf
@@ -22,11 +22,9 @@ resource "azurerm_linux_function_app" "bhmcitafalinux01" {
 
 resource "azurerm_storage_account" "bhmcitstorage" {
   name                       = "bhstorageaccount"
-  storage_account_name       = azurerm_storage_account.bhmcitstorage.name
-  location                   = azurerm_resource_group.bhmcitstorage.location
+  resource_group_name        = azurerm_resource_group.bhmcitrg01.name
+  location                   = azurerm_resource_group.bhmcitrg01.location
   account_tier               = "Standard"
   account_replication_type   = "GRS"
-  storage_account_access_key = azurerm_storage_account.bhmcitstorage.primary_access_key
-  service_plan_id            = azurerm_service_plan.bhmcitstorage.id
 }
 
