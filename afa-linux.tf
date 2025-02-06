7ecb2300f26440f1f6b2f839c3bdeb893efae021
bholmes-learning <bh.learning.studies@gmail.com>
2025-02-05 20:13:34 -0500

    Update afa-linux.tf

--- afa-linux.tf
+++ afa-linux.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "bhmcitrg01" {
+  name     = "mcit_resource_group_bh01"
+  location = "canadacentral"
+}
+
 resource "azurerm_storage_account" "bhmcitrg01" {
   name                     = "braedenmcitlinuxfnappsa"
   resource_group_name      = azurerm_resource_group.bhmcitrg01.name
@@ -6,21 +11,24 @@ resource "azurerm_storage_account" "bhmcitrg01" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_service_plan" "bhmcitasplinux01" {
+  name                = "braedenmcitappserviceplan"
+  resource_group_name = azurerm_resource_group.bhmcitrg01.name
+  location            = azurerm_resource_group.bhmcitrg01.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
 resource "azurerm_linux_function_app" "bhmcitafalinux01" {
   name                = "braedenmcitfunction"
   resource_group_name = azurerm_resource_group.bhmcitrg01.name
   location            = azurerm_resource_group.bhmcitrg01.location
-  service_plan_id     = azurerm_service_plan.bhmcitasplinux01.id
+
+  storage_account_name       = azurerm_storage_account.bhmcitrg01.name
+  storage_account_access_key = azurerm_storage_account.bhmcitrg01.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
 
   site_config {}
 }
 
-resource "azurerm_storage_account" "bhmcitstorage" {
-  name                       = "bhstorageaccount"
-  resource_group_name        = azurerm_resource_group.bhmcitrg01.name
-  location                   = azurerm_resource_group.bhmcitrg01.location
-  account_tier               = "Standard"
-  account_replication_type   = "GRS"
-  
-}
 
