fdf04d247d4ac9535b209adb912992ff7ea07b08
bholmes-learning <bh.learning.studies@gmail.com>
2025-02-05 20:06:37 -0500

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "bhmcitrg01" {
   name     = "mcit_resource_group_bh01"
   location = "canadacentral"
 }
+
+resource "azurerm_service_plan" "bhmcitasp01" {
+  name     = "braedenmcitazsp01"
+  resource_group_name = azurerm_resource_group.bhmcitrg01.name
+  location            = azurerm_resource_group.bhmcitrg01.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
